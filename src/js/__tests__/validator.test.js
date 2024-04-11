22054cd54419f449768d399a6a87a750319300b4
JulieSoboleva <ju.soboll@gmail.com>
2024-04-11 09:06:01 +0300

    Add tests

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,12 +1,51 @@
 import Validator from '../validator';
-import validateUserName from '../validator';
 
-test('check not valid userName', () => {
-  const validator = new Validator('_user1234_');
+test('Проверить вариант с цифрой в начале', () => {
+  const validator = new Validator('1user');
   expect(validator.validateUserName()).toBe(false);
 });
 
-test('check valid userName', () => {
-  const validator = new Validator('user123_456-69e');
+test('Проверить вариант с цифрой в конце', () => {
+  const validator = new Validator('user2');
+  expect(validator.validateUserName()).toBe(false);
+});
+
+test('Проверить вариант с - в начале слова', () => {
+  const validator = new Validator('-user');
+expect(validator.validateUserName()).toBe(false);
+});
+
+test('Проверить вариант с - в конце в слова', () => {
+  const validator = new Validator('user-');
+expect(validator.validateUserName()).toBe(false);
+});
+
+test('Проверить вариант с _ в начале слова', () => {
+  const validator = new Validator('_user');
+expect(validator.validateUserName()).toBe(false);
+});
+test('Проверить вариант с _ в конце слова', () => {
+  const validator = new Validator('user_');
+expect(validator.validateUserName()).toBe(false);
+});
+test('Проверить вариант с 4-мя цифрами подряд в середине слова', () => {
+  const validator = new Validator('user1234user');
+expect(validator.validateUserName()).toBe(false);
+});
+
+test('Проверить вариант c - в середине слова', () => {
+  const validator = new Validator('user123-69e');
   expect(validator.validateUserName()).toBe(true);
 });
+
+test('Проверить вариант с _ в середине слова', () => {
+  const validator = new Validator('user987_7f');
+  expect(validator.validateUserName()).toBe(true);
+});
+
+test('Проверить вариант с 3-мя цифрами подряд в середине слова', () => {
+  const validator = new Validator('user123user');
+  expect(validator.validateUserName()).toBe(true);
+});
+
+
